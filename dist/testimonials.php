40c815aadf7710f96708b1457e4b1a5cfaab8a86
desmes <antoine.thiri@gmail.com>
2025-04-23 09:33:54 +0200

    Add testimonials page and clean up menu styles
    - Created a new testimonials.php page with basic structure and included necessary components.
    - Removed unused styles for the menu in __menu.scss to streamline the code.

---
+++ dist/testimonials.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Coaching sportif professionnel, individuel et en groupe. Découvrez nos services adaptés à vos besoins et vos objectifs.">
+	<title>Témoignages</title>
+	<link rel="stylesheet" href="style/min_css/main.min.css">
+</head>
+
+<body class="page__accueil" data-page="testimonials" data-variant="black" data-module="faq">
+
+	<div class="wrapper-1440">
+		<?php
+		include 'components/__menu.php';
+		?>
+	</div>
+
+	<main class="main">
+
+	</main>
+	<?php
+	include 'components/__faq.php';
+	?>
+	<?php
+	include 'components/__footer.php';
+	?>
+</body>
+
+<script type="module" src="js/global/main.js"></script>
+<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+
+</html>
